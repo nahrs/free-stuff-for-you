21289579f97840d97caa426e341abe64dfc768b7
AnonyHelper <55519887+AnonyHelper@users.noreply.github.com>
2019-09-18 23:11:29 -0400

    Add files via upload

---
+++ edhesive/Lesson-1/Lesson_1_Activity_One.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+class Lesson_1_Activity_One {
+    public static void main(String[] args) {
+      
+        System.out.print("  Your Name");
+
+    }
+}
---
+++ edhesive/Lesson-1/Lesson_1_Activity_Two.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+class Lesson_1_Activity_Two {
+    public static void main(String[] args) {
+      
+        System.out.print(" Your Name\n Your Name\n Your Name");
+	//The \n creates a new line within a string. \ Is code within string so it will not 
+	//normally work as text, unless typed as \\.
+    }
+}
