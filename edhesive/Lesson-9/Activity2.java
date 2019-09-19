827ca8d1f9900c7efd0432a76943a73d600be976
Rayool <35053383+NAHRS@users.noreply.github.com>
2019-09-19 19:30:19 -0400

    Create Activity2.java

---
+++ edhesive/Lesson-9/Activity2.java
@@ -0,0 +1,11 @@
+import java.lang.Math;
+
+class Lesson_9_Activity_Two{
+     public static void main(String[] args){
+         int rand = (int) ((Math.random() * 20) - 20);
+         System.out.println(rand);
+     }    
+    
+    
+    
+}
