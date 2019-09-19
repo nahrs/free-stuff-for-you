be0f148b698f0265226d4bfad2a973a6b9cb7fef
Rayool <35053383+NAHRS@users.noreply.github.com>
2019-09-19 19:27:39 -0400

    Create Activity1.java

---
+++ edhesive/Lesson-9/Activity1.java
@@ -0,0 +1,11 @@
+import java.lang.Math;
+
+class Lesson_9_Activity_One{
+     public static void main(String[] args){
+         int rand = (int) ((Math.random() * 10) + 1);
+         System.out.println(rand);
+     }    
+    
+    
+    
+}
