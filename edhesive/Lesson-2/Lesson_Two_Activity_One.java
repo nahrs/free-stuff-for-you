19ce0e8b33de1d75a8d8cb42190aa8fddd929c3f
AnonyHelper <55519887+AnonyHelper@users.noreply.github.com>
2019-09-18 23:42:48 -0400

    Add files via upload

---
+++ edhesive/Lesson-2/Lesson_Two_Activity_One.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+class Lesson_2_Activity_One {
+    public static void main(String[] args) {
+      
+        System.out.print("/\\\n\\/");
+
+    }
+}
---
+++ edhesive/Lesson-2/Lesson_Two_Activity_Three.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+class Lesson_2_Activity_Three {
+    public static void main(String[] args) {
+      
+        System.out.print("   _\n /   \\\n|     |\n \\ _ /");
+
+    }
+}
