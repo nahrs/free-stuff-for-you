6fee341546aabfb4b5c0c1f11278fac8602d5d35
Rayool <35053383+NAHRS@users.noreply.github.com>
2019-09-19 21:43:32 -0400

    Update Activity2.java

--- edhesive/Lesson-9/Activity2.java
+++ edhesive/Lesson-9/Activity2.java
@@ -2,10 +2,8 @@ import java.lang.Math;
 
 class Lesson_9_Activity_Two{
      public static void main(String[] args){
-         int rand = (int) ((Math.random() * 20) - 20);
+         int rand = (int) ((Math.random() * 41) - 20);
          System.out.println(rand);
      }    
-    
-    
     
 }
