3a8a3eae1ed00dcf74f409e434a6dfb5183fe330
Rayool <35053383+NAHRS@users.noreply.github.com>
2019-10-07 19:04:03 -0400

    Create Activity1.java

---
+++ edhesive/Term-2/Lesson-11/Activity1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+class Lesson_11_Activity_One{
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        
+        int x = s.nextInt();
+        
+        if(x == 176){
+            System.out.println("YES");
+            
+        }
+        
+        
+    }    
+    
+    
+}
