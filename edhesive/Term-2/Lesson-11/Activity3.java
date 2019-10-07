4b0bdd700eba5606adee7add78966a58a77c06d1
Rayool <35053383+NAHRS@users.noreply.github.com>
2019-10-07 19:05:00 -0400

    Update Activity3.java

--- edhesive/Term-2/Lesson-11/Activity3.java
+++ edhesive/Term-2/Lesson-11/Activity3.java
@@ -1,1 +1,20 @@
+import java.util.Scanner;
+import java.lang.Math;
 
+class Lesson_11_Activity_Three{
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        
+        int x = s.nextInt();
+        int y = s.nextInt();
+        
+        if(x == y){
+            System.out.println("YES");
+            
+        }
+        
+        
+    }    
+    
+    
+} 
